c30ef8047e54bcc4ae1830fc33876cebcd2646d2
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-13 11:15:34 -0300

    Connecting to MongoDB using mongoose.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://mongo:27017/nodeapi", { useNewUrlParser : true });
+
+const app = express();
 app.get("/", (request, response) => response.send("Hello world!"));
 app.listen(3000);
