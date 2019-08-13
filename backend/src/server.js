2044531dfe5a734920bc461ed5c115e888bfdc40
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-13 14:07:12 -0300

    Importing all the DB schemas from the models directory.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 mongoose.connect("mongodb://mongo:27017/nodeapi", { useNewUrlParser : true });
+requireDir("./models");
+
+const Product = mongoose.model("Product");
 
 const app = express();
-app.get("/", (request, response) => response.send("Hello world!"));
+app.get("/", (request, response) => {
+    // Product.create({
+    //     title: "React Native",
+    //     description: "Build native apps with React",
+    //     url: "http://github.com/facebook/react-native"
+    // });
+    response.send("Hello world!")
+});
 app.listen(3000);
