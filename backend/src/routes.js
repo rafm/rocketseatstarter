56241234b86320bccc9cbdb4573f3efa2196ced0
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-13 19:36:18 +0000

    Restructuring the code: adding Controller and Routes management in separated JS files.

---
+++ backend/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const routes = express.Router();
+
+const ProductsController = require('./controllers/ProductController');
+routes.get('/products', ProductsController.index);
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,15 +5,6 @@ const requireDir = require("require-dir");
 mongoose.connect("mongodb://mongo:27017/nodeapi", { useNewUrlParser : true });
 requireDir("./models");
 
-const Product = mongoose.model("Product");
-
-const app = express();
-app.get("/", (request, response) => {
-    // Product.create({
-    //     title: "React Native",
-    //     description: "Build native apps with React",
-    //     url: "http://github.com/facebook/react-native"
-    // });
-    response.send("Hello world!")
-});
-app.listen(3000);
+express()
+    .use('/api', require("./routes"))
+    .listen(3000);
