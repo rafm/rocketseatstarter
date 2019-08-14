8f6b903046a5d9352419406ada513103b096a05a
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-14 17:14:53 +0000

    Configuring CORS for any other application be able to use the endpoints.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 mongoose.connect("mongodb://mongo:27017/nodeapi", { useNewUrlParser : true });
 requireDir("./models");
 
 express()
     .use(express.json())
+    .use(cors())
     .use('/api', require("./routes"))
     .listen(3000);
