053c1c51f4e03d1a0f40eeb8895e170d055e8bf9
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-10-31 21:40:25 +0000

    frontend: creating Header component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
+const App = () =>
     <div className="App">
-      <h1>Hello Rocketseat</h1>
+        <Header />
     </div>
-  );
-}
 
 export default App;
