94aeff914feffbc5c8e1205b9582785e2a101746
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-11-01 21:23:11 +0000

    frontend: listing products from the API with pagination.

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+        productInfo: {},
+    };
+
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        console.log(response.data.docs);
-    }
+        const { docs: products, ...productInfo } = response.data;
+
+        this.setState({ products, productInfo });
+    };
+
+    prevPage = () => {
+        const { productInfo } = this.state;
+        const page = parseInt(productInfo.page);
+
+        if (page === 1) return;
+
+        this.loadProducts(page - 1);
+    };
+
+    nextPage = () => {
+        const { productInfo } = this.state;
+        const page = parseInt(productInfo.page);
+
+        if (page === productInfo.pages) return;
+
+        this.loadProducts(page + 1);
+    };
 
     render() {
-        return <h1>Hello Rocketseat</h1>;
+        const { products, productInfo } = this.state;
+
+        return <div className="product-list">
+            {products.map(product =>
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                </article>
+            )}
+            <div className="actions">
+                <button disabled={parseInt(productInfo.page) === 1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={parseInt(productInfo.page) === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+            </div>
+        </div>;
     }
 }
